636d2cea1f6cc1cdce61cab7fa50eb158e074926
Chaos <panjiazao@163.com>
2023-03-24 20:28:31 +0800

    更新 CoexControl.cs
    添加部分代码

--- Assets/Scripts/GameSence/World/Market/CoexControl.cs
+++ Assets/Scripts/GameSence/World/Market/CoexControl.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace World
 {
@@ -8,9 +9,13 @@ namespace World
     /// </summary>
     public class CoexControl : MonoBehaviour
     {
+        public Text taskContentText;
+        public Text rewardText;
+
         private void OnEnable()
         {
             AudioControl.Instance.PlayBackgroundMusic(AudioControl.BackgroundMusicType.World);
+            OnTaskPanel();
         }
         /// <summary>
         /// 点击了返回按钮
@@ -19,6 +24,30 @@ namespace World
         {
             AudioControl.Instance.PlayBackgroundMusic(AudioControl.BackgroundMusicType.World);
             gameObject.SetActive(false);
+        }
+
+        //增加任务面板功能，可以显示、接取任务。任务内容随机生成，内容主要是指定天数内某个属性达到某个数值，任务奖励是奖励属性数值
+        public void OnTaskPanel()
+        {
+            //显示任务列表
+            //生成随机任务
+            int days = UnityEngine.Random.Range(1, 7);
+            int attribute = UnityEngine.Random.Range(1, 4);
+            int value = UnityEngine.Random.Range(10, 21);
+            string taskContent = $"在{days}天内将{attribute}属性提升至{value}";
+
+            //显示任务内容
+            taskContentText.text = $"任务内容：{taskContent}";
+
+
+            //奖励属性数值
+            rewardText.text = $"奖励{attribute}属性{value}点";
+        }
+        
+        //接取任务
+        private void OnFetchTask()
+        {
+        
         }
     }
 }
