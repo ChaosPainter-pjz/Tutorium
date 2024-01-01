631b20b2c74e57a597cc7d2e891a9fb101ae9635
Chaos <panjiazao@163.com>
2024-01-01 16:37:50 +0800

    刷新新闻的日期
    偷懒了，只是换了年份，没有新的新闻

--- Assets/Scripts/GameSence/World/WorldManager.cs
+++ Assets/Scripts/GameSence/World/WorldManager.cs
@@ -33,6 +33,7 @@ public class WorldManager : MonoBehaviour
         mainPanel.localScale = Vector3.one;
         worldPanel.SetActive(false);
     }
+
     /// <summary>
     /// 点击外出按钮
     /// </summary>
@@ -45,62 +46,49 @@ public class WorldManager : MonoBehaviour
 
     public void UnderConstruction()
     {
-        HintManager.Instance.AddHint(new Hint("施工中","该商户还在装修中，尚未开业"));
+        HintManager.Instance.AddHint(new Hint("施工中", "该商户还在装修中，尚未开业"));
     }
 
     public void OnKtv()
     {
-        if (gameManager.saveObject.SaveData.gameDate.year==gameManager.saveObject.SaveData.InitYear)
-        {
-            HintManager.Instance.AddHint(new Hint("施工即将完成","工作人员告诉你：该商户还在试营业中，将在明年第一周开业"));
-        }
+        if (gameManager.saveObject.SaveData.gameDate.year == gameManager.saveObject.SaveData.InitYear)
+            HintManager.Instance.AddHint(new Hint("施工即将完成", "工作人员告诉你：该商户还在试营业中，将在明年第一周开业"));
         else
-        {
             ktvControl.gameObject.SetActive(true);
-        }
     }
 
     public void OnSupermarket()
     {
-        if (gameManager.saveObject.SaveData.gameDate.year == gameManager.saveObject.SaveData.InitYear && gameManager.saveObject.SaveData.gameDate.Semester == 0)
-        {
+        if (gameManager.saveObject.SaveData.gameDate.year == gameManager.saveObject.SaveData.InitYear &&
+            gameManager.saveObject.SaveData.gameDate.Semester == 0)
             HintManager.Instance.AddHint(new Hint("施工即将完成", "工作人员告诉你：该商户还在试营业中，将在今年下学期开业"));
-        }
         else
-        {
             supermarketControl.gameObject.SetActive(true);
-        }
     }
+
     /// <summary>
     /// 点击会展中心
     /// </summary>
     public void OnCoex()
     {
-        if (IsCoex())
-        {
+        //if (IsCoex())
+        if (false)
             coexControl.gameObject.SetActive(true);
-        }
         else
-        {
             HintManager.Instance.AddHint(new Hint("施工即将完成", "工作人员告诉你：该商户还在装修中，尚不知晓何时开业"));
-        }
     }
+
     /// <summary>
     /// 返回现在会展广场是否开业了
     /// </summary>
     /// <returns></returns>
     private bool IsCoex()
     {
-        if (gameManager.saveObject.SaveData.gameDate.year < gameManager.saveObject.SaveData.InitYear + 1)
-        {
-            return false;
-        }
+        if (gameManager.saveObject.SaveData.gameDate.year < gameManager.saveObject.SaveData.InitYear + 1) return false;
 
         if (gameManager.saveObject.SaveData.gameDate.year == gameManager.saveObject.SaveData.InitYear + 1
             && gameManager.saveObject.SaveData.gameDate.Semester == 0)
-        {
             return false;
-        }
 
         return true;
     }
